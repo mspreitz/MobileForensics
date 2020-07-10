0b1d5a0fc48a48fe4af111de2b22156e2b6d0115
Michael Spreitzenbarth <research@spreitzenbarth.de>
2020-07-10 09:20:24 +0200

    scripts updated for Python3

--- AndroidForensics/Screenlock/crack_screenlock_gesture.py
+++ AndroidForensics/Screenlock/crack_screenlock_gesture.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
-#
-# Copyright (C) 2015 Michael Spreitzenbarth (research@spreitzenbarth.de)
+#!/usr/bin/python3
+
+#############################################################################################
+# Copyright (C) 2020 Michael Spreitzenbarth (research@spreitzenbarth.de)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,33 +15,48 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################################
+
 import hashlib, sqlite3, os, sys, subprocess, struct
 from binascii import hexlify
 
+
 SQLITE_DB = "GestureRainbowTable.db"
 
+
 def crack(backup_dir):
+
     # dumping the system file containing the hash
-    print "Dumping gesture.key ..."
-    saltdb = subprocess.Popen(['adb', 'pull', '/data/system/gesture.key', backup_dir], 
+    print("[i] dumping gesture.key ...")
+    gesture = subprocess.run(['adb', 'pull', '/data/system/gesture.key', backup_dir], 
         stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    gesturehash = open(backup_dir + "/gesture.key", "rb").readline()
-    lookuphash = hexlify(gesturehash).decode()
-    print "HASH: \033[0;32m" + lookuphash + "\033[m"
+    if gesture.returncode == 0:
+        gesturehash = open(backup_dir + "/gesture.key", "rb").readline()
+        lookuphash = hexlify(gesturehash).decode()
+        print("[i] found hash: \033[0;32m" + lookuphash + "\033[m")
 
-    conn = sqlite3.connect(SQLITE_DB)
-    cur = conn.cursor()
-    cur.execute("SELECT pattern FROM RainbowTable WHERE hash = ?", (lookuphash,))
-    gesture = cur.fetchone()[0]
+        conn = sqlite3.connect(SQLITE_DB)
+        cur = conn.cursor()
+        cur.execute("SELECT pattern FROM RainbowTable WHERE hash = ?", (lookuphash,))
+        gesture = cur.fetchone()[0]
 
-    return gesture
+        return gesture
+    else:
+        print("[!] " + gesture.stdout.decode("utf-8").split("\n")[0])
+        print("[!] exiting...")
+        sys.exit(2)
+
 
 if __name__ == '__main__':
+    print("[i] starting...")
 
     # check if device is connected and adb is running as root
-    if subprocess.Popen(['adb', 'get-state'], stdout=subprocess.PIPE).communicate(0)[0].split("\n")[0] == "unknown":
-        print "no device connected - exiting..."
+    deviceAvailable = subprocess.run(['adb', 'get-state'], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if deviceAvailable.returncode != 0:
+        print("[!] " + deviceAvailable.stderr.decode("utf-8").split("\n")[0])
+        print("[!] exiting...")
         sys.exit(2)
 
     # starting to create the output directory and the crack file used for hashcat
@@ -52,5 +68,4 @@ if __name__ == '__main__':
         os.mkdir(backup_dir)
 
     gesture = crack(backup_dir)
-
-    print "Screenlock Gesture: \033[0;32m" + gesture + "\033[m"
+    print("[i] screenlock gesture: \033[0;32m" + gesture + "\033[m")
--- AndroidForensics/Screenlock/crack_screenlock_pin.py
+++ AndroidForensics/Screenlock/crack_screenlock_pin.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
-#
-# Copyright (C) 2015 Michael Spreitzenbarth (research@spreitzenbarth.de)
+#!/usr/bin/python3
+
+#############################################################################################
+# Copyright (C) 2020 Michael Spreitzenbarth (research@spreitzenbarth.de)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,6 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################################
 
 import os, sys, subprocess, binascii, struct
 import sqlite3 as lite
@@ -22,31 +24,29 @@ import sqlite3 as lite
 def get_sha1hash(backup_dir):
 
     # dumping the password/pin from the device
-    print "Dumping PIN/Password hash ..."
-    password = subprocess.Popen(['adb', 'pull', '/data/system/password.key', backup_dir], 
+    print("[i] dumping PIN/Password hash ...")
+    password = subprocess.run(['adb', 'pull', '/data/system/password.key', backup_dir], 
         stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    password.wait()
 
     # cutting the HASH within password.key
-    sha1hash = open(backup_dir + '/password.key', 'r').readline()[:40]
-    print "HASH: \033[0;32m" + sha1hash + "\033[m"
+    if password.returncode == 0:
+        sha1hash = open(backup_dir + '/password.key', 'r').readline()[:40]
+        print("[i] hash: \033[0;32m" + sha1hash + "\033[m")
     
-    return sha1hash
+        return sha1hash
+    else:
+        print("[!] " + password.stdout.decode("utf-8").split("\n")[0])
+        print("[!] exiting ...")
+        sys.exit(2)
 
 
 def get_salt(backup_dir):
 
     # dumping the system DB containing the SALT
-    print "Dumping locksettings.db ..."
-    saltdb = subprocess.Popen(['adb', 'pull', '/data/system/locksettings.db', backup_dir], 
-        stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    saltdb.wait()
-    saltdb2 = subprocess.Popen(['adb', 'pull', '/data/system/locksettings.db-wal', backup_dir], 
-        stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    saltdb2.wait()
-    saltdb3 = subprocess.Popen(['adb', 'pull', '/data/system/locksettings.db-shm', backup_dir], 
-        stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    saltdb3.wait()
+    print("[i] dumping locksettings.db ...")
+    for file in ['locksettings.db', 'locksettings.db-wal', 'locksettings.db-shm']:
+        saltdb = subprocess.run(['adb', 'pull', '/data/system/' + file, backup_dir], 
+            stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # extract the SALT
     con = lite.connect(backup_dir + '/locksettings.db')
@@ -56,15 +56,19 @@ def get_salt(backup_dir):
     con.close()
 
     # convert SALT to Hex
-    returnedsalt =  binascii.hexlify(struct.pack('>q', int(salt) ))
-    print "SALT: \033[0;32m" + returnedsalt + "\033[m"
+    returnedsalt =  binascii.hexlify(struct.pack('>q', int(salt))).decode("utf-8")
+    print("[i] salt: \033[0;32m" + returnedsalt + "\033[m")
 
     return returnedsalt
 
 
 def write_crack(salt, sha1hash, backup_dir):
 
-    crack = open(backup_dir + '/crack.hash', 'a+')
+    file = backup_dir + '/crack.hash'
+    if os.path.exists(file):
+        print("[!] crack.hash will be overwritten!")
+        os.remove(file)
+    crack = open(file, 'a+')
     
     # write HASH and SALT to cracking file
     hash_salt = sha1hash + ':' + salt
@@ -73,10 +77,14 @@ def write_crack(salt, sha1hash, backup_dir):
 
     
 if __name__ == '__main__':
+    print("[i] starting ...")
 
     # check if device is connected and adb is running as root
-    if subprocess.Popen(['adb', 'get-state'], stdout=subprocess.PIPE).communicate(0)[0].split("\n")[0] == "unknown":
-        print "no device connected - exiting..."
+    deviceAvailable = subprocess.run(['adb', 'get-state'], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if deviceAvailable.returncode != 0:
+        print("[!] " + deviceAvailable.stderr.decode("utf-8").split("\n")[0])
+        print("[!] exiting ...")
         sys.exit(2)
 
     # starting to create the output directory and the crack file used for hashcat
@@ -91,5 +99,5 @@ if __name__ == '__main__':
     salt = get_salt(backup_dir)
     write_crack(salt, sha1hash, backup_dir)
 
-    print "crack.hash can now be used to feed hashcat"
-    print "\033[0;32m-> hashcat -a 3 -m 110 " + backup_dir + "/crack.hash -1 ?d ?1?1?1?1\033[m (just add ?1 for echt additional digit to crack)"
+    print("[i] crack.hash can now be used to feed hashcat:")
+    print("[i] \033[0;32m-> hashcat -a 3 -m 110 " + backup_dir + "/crack.hash -1 '?d' '?1?1?1?1' --show\033[m (just add ?1 for each additional digit to crack)")
